415f2a2c74e2f93852532face804fcc3e7c2ebc6
zeryx <james@algorithmia.com>
2021-01-17 14:52:37 -0400

    added functioning loading spinner, error messages now get reported to text box

--- js/content.js
+++ js/content.js
@@ -11,7 +11,7 @@ function getSelectionText() {
     return text;
 }
 
-function createTextBox() {
+function createTextBox(loading_elm) {
     var height = '50px';
     var button_width = '50px';
     var box = document.createElement('div');
@@ -22,6 +22,7 @@ function createTextBox() {
     box.style.height = height;
     box.style.opacity = '0.80';
     box.style.zIndex = '99999';
+
     var area = document.createElement('textarea');
     area.style.position = 'absolute';
     area.style.width = '95%';
@@ -30,6 +31,7 @@ function createTextBox() {
     area.style.background = 'lightblue';
     area.style.opacity = 'parent';
     area.readOnly = true;
+
     var button = document.createElement('button');
     button.id = 'button';
     button.style.position = 'absolute';
@@ -38,6 +40,7 @@ function createTextBox() {
     button.style.color = 'black';
     button.style.opacity = 'parent';
     button.style.right = '20px';
+
     button.onclick =function () {
         console.log(selection);
         selection = iterateSelector(selection);
@@ -45,10 +48,29 @@ function createTextBox() {
             updateBoxContent(box);
         }
     };
+
     box.appendChild(area);
     box.appendChild(button);
-    // document.body.appendChild(box);
+    box.appendChild(loading_elm);
     return box;
+}
+
+function createSpinner() {
+    const height = "20px"
+    const width = "20px"
+    var spinner = document.createElement('div');
+    spinner.style.border = "16px solid #f3f3f3";
+    spinner.style.position = 'absolute';
+    spinner.style.height = height;
+    spinner.style.width = width;
+    spinner.style.borderTop = "16px solid #3498db";
+    spinner.style.borderRadius = "50%";
+    spinner.animate([
+        {transform: 'rotate(0deg)'},
+        {transform: 'rotate(360deg)'}
+    ], {duration: 1000, iterations: Infinity});
+    spinner.style.visibility = "hidden";
+    return spinner
 }
 
 function toggleTextBox(box, status) {
@@ -59,11 +81,24 @@ function toggleTextBox(box, status) {
     }
 }
 
-function updateBoxContent(box, value) {
-    if (value != null){
-        CURRENT_RESULT = value;
+function toggleLoading(box, spinner, status) {
+    if (status === false) {
+        spinner.style.visibility = "hidden";
+    } else {
+        spinner.style.visibility = "visible";
     }
-    box.children[0].innerText = CURRENT_RESULT[selection];
+}
+
+
+function updateBoxContent(box, value, err) {
+    if(err == null) {
+        if (value != null) {
+            CURRENT_RESULT = value;
+        }
+        box.children[0].innerText = CURRENT_RESULT[selection];
+    } else {
+        box.children[0].innerText = JSON.stringify(value);
+    }
 }
 
 function iterateSelector(i) {
@@ -83,18 +118,21 @@ browser.runtime.onMessage.addListener(request => {
         if (visible_box === true) {
             const text = getSelectionText();
             if (text) {
-                console.log("selected: " + text);
                 updateBoxContent(box, {0:"", 1:"", 2:""});
+                toggleLoading(box, spinner, true)
                 browser.runtime.sendMessage({"content": text}).then(
                     function(result) {
                         console.log(JSON.stringify(result));
-                        // result = Object.values(result);
+                        toggleLoading(box, spinner, false)
                         updateBoxContent(box, result);
                     },
-                    function (e) {console.err(e)}
+                    function (e) {
+                        toggleLoading(box, spinner, false)
+                        updateBoxContent(box, e, true)
+                    }
                 );
             } else {
-                console.log("hotkey was pressed without highlighted text")
+                updateBoxContent(box, "hotkey was pressed without highlighted text", true)
             }
         } else {
             console.log("command pressed but not valid.")
@@ -106,4 +144,5 @@ var selection = 0;
 var CURRENT_RESULT = {0:"", 1:"", 2:""};
 var options = [0, 1, 2];
 var visible_box = false;
-var box = createTextBox();
+var spinner = createSpinner();
+var box = createTextBox(spinner);
